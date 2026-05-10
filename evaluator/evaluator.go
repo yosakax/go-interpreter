1cc8f7885d981e6552e92f314df0eaafeed8c7ac
yosaka <fallingfluit@gmail.com>
2026-05-10 17:27:38 +0900

    feat: implement basic evaluator for integer and boolean expressions
    - Add evaluator package with Eval function supporting Program, ExpressionStatement, IntegerLiteral, and Boolean nodes
    - Add tests for integer and boolean evaluation
    - Integrate evaluator into REPL to display evaluated results

---
+++ evaluator/evaluator.go
@@ -0,0 +1,48 @@
+package evaluator
+
+import (
+	"yosaka/go-interpreter/ast"
+	"yosaka/go-interpreter/object"
+)
+
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// 文
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// 式
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
