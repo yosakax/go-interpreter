c688adf4bbe133d1491fcaa39b92b58757ba6a0b
yosaka <fallingfluit@gmail.com>
2026-05-12 01:46:31 +0900

    feat: add support for user-defined functions and function application
    - Implemented function literals and function application in the evaluator.
    - Added Function object type, including parameter, body, and closure environment handling.
    - Supported lexical scoping via enclosed environments.
    - Added tests for function objects and function application.
    - Updated environment to support nested scopes.

--- object/environment.go
+++ object/environment.go
@@ -2,10 +2,14 @@ package object
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
@@ -17,4 +21,10 @@ func (e *Environment) Set(name string, val Object) Object {
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{store: s}
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
